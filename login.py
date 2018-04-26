df2a95d3fed15f72246d3c950c5ea6897d96b59d
SkyKnight <37358336+SkyKnight-Team@users.noreply.github.com>
2018-04-26 17:08:57 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,50 @@
+from colorama import Fore, Back, init
+import sys, os, time
+from getpass import getpass
+animation = "|/-\\"
+animation3 = "/-|\\"
+animation4 = "/x/x\\x"
+animation5 = "%÷%÷"
+os.system("clear")
+
+
+
+m = []
+
+f = open("/data/data/com.termux/files/usr/security/log.txt","r")
+b = f.read()
+
+#c = len(b)
+#a.append(b)
+l = b.split("\n")
+print(Fore.RED+"Log In : \n")
+
+usr = input(Fore.GREEN+"User Id : "+Fore.CYAN+"")
+m.append(usr)
+
+#print(a)
+if l[2] == m[0] :
+    print(Fore.GREEN+"\nUser Id Matched. \n")
+    passwd = getpass(Fore.GREEN+"\nPassword : "+Fore.CYAN+"")
+    m.append(passwd)
+    if l[3] == m[1]:
+        print(Fore.GREEN+"\nPassword Matched.\n ")
+        for i in range(100):
+            #sys.stdout.write(Fore.GREEN+"Logging in..... ")
+            time.sleep(0.1)
+            sys.stdout.write(Fore.YELLOW+"\r" + animation[i % len(animation)])
+            sys.stdout.write(Fore.GREEN+" ........Logging....... ")
+            sys.stdout.write(Fore.YELLOW+"" + animation[i % len(animation)]) 
+            sys.stdout.flush()
+            #do something
+        os.system("clear")
+        print(Fore.YELLOW+"\t[  Wecome ===> "+l[0]+"  ]")
+        init(autoreset=True)
+    else:
+        print(Fore.RED+"Password Not Matched.")
+        time.sleep(3)
+        os.system("killall -9 com.termux")
+else:
+    print(Fore.RED+"User Id Not Matched.")
+    time.sleep(3)
+    os.system("killall -9 com.termux")
---
+++ register.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import os
+import sys
+import time
+from colorama import Fore, Back, init
+import string as s
+from getpass import getpass
+
+a = s.digits
+b = s.ascii_letters
+c = s.punctuation
+
+logo = '''
+ _____ _____ _                
+|_   _|____ | |               
+  | |     / / |     ___   ___ 
+  | |     \ \ |    / _ \ / __|
+  | | .___/ / |___| (_) | (__ 
+  \_/ \____/\_____/\___/ \___|
+                              
+'''
+about = '''
+Author       : EPIC                      | Tool Name    : T3Loc
+Team         : SkyKnight                 | Tool Version : V1.0
+Date         : 26/04/2018                | Made By      : EPIC
+Time         : 4:23 PM (India)           |
+Author Email : epic.skyknight@gmail.com  |
+-----------------------------------------------------------------
+'''
+print(Fore.CYAN+logo)
+print(Fore.YELLOW+about)
+def register():
+    #os.system("clear")
+    print(Fore.RED+"           \n\nRegister :\n\t")
+    fname = input(Fore.GREEN+"First Name :"+Fore.YELLOW+"")
+    if fname not in a+b+c:
+        print(Fore.CYAN+"\nSuccessfully Add\n")
+        lname = input(Fore.GREEN+"Last Name  :"+Fore.YELLOW+"")
+        if lname not in a+b+c:
+            print(Fore.CYAN+"\nSuccessfully Add\n")
+            usr = input(Fore.GREEN+"User Id    :"+Fore.YELLOW+"")
+            if usr not in a+b+c:
+                print(Fore.CYAN+"\nSuccessfully Add\n")
+                passwd = getpass(Fore.GREEN+"Password   :"+Fore.YELLOW+"")
+                if passwd not in a+b+c:
+                    print(Fore.CYAN+"\nSuccessfully Add\n")
+                    passwd1 = getpass(Fore.GREEN+"Confirm Password :"+Fore.YELLOW+"")
+                    if passwd == passwd1:
+                        print(Fore.CYAN+"\nSuccessfully Confirm Password\n")
+                        os.system("mkdir /data/data/com.termux/files/usr/security")
+                        f = open("/data/data/com.termux/files/usr/security/log.txt","w")
+                        f.write(fname+"\n")
+                        f.close()
+                        s = open("/data/data/com.termux/files/usr/security/log.txt","a")
+                        s.write(lname+"\n")
+                        s.write(usr+"\n")
+                        s.write(passwd+"\n")
+                        s.write(passwd1+"\n")
+                        s.close()
+                        
+                    else:
+                        print(Fore.RED+"Please Enter Correct Password.")
+                else:
+                    print(Fore.RED+"Please Enter Password.\n")
+            else:
+                print(Fore.RED+"Please Enter User Id.\n")
+        else:
+            print(Fore.RED+"Please Enter Last Name.\n")
+    else:
+        print(Fore.RED+"Please Enter First Name.\n")
+register() 
+init(autoreset=True) 
+os.system("mv register.py /data/data/com.termux/files/usr/security")  
+   
